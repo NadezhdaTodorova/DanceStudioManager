ef029e3966a635faa08100d27199dc37c7ccb4cc
Nadezhda Todorova (Crossroad) <ntodorova@crossroad.bg>
2019-07-23 17:02:40 +0300

    fixing the login

--- DanceStudioManager/wwwroot/js/RegisterLogin.js
+++ DanceStudioManager/wwwroot/js/RegisterLogin.js
@@ -7,16 +7,8 @@
     $('.register-show').addClass('show-log-panel');
 
     $('.login-reg-panel input[type="radio"]').on('change', function() {
-        if($('#log-login-show').is(':checked')) {
-            $('.register-info-box').fadeOut(); 
-            $('.login-info-box').fadeIn();
 
-            $('.white-panel').addClass('right-log');
-            $('.register-show').addClass('show-log-panel');
-            $('.login-show').removeClass('show-log-panel');
-
-        }
-        else if($('#log-reg-show').is(':checked')) {
+        if ($('#log-reg-show').is(':checked')) {
             $('.register-info-box').fadeIn();
             $('.login-info-box').fadeOut();
 
@@ -25,6 +17,35 @@
             $('.login-show').addClass('show-log-panel');
             $('.register-show').removeClass('show-log-panel');
         }
+        else if ($('#log-login-show').is(':checked')) {
+            $('.register-info-box').fadeOut(); 
+            $('.login-info-box').fadeIn();
+
+            $('.white-panel').removeClass('right-log');
+            $('.register-show').addClass('show-log-panel');
+            $('.login-show').removeClass('show-log-panel');
+
+        }
+
+
+        //if($('#log-login-show').is(':checked')) {
+        //    $('.register-info-box').fadeOut(); 
+        //    $('.login-info-box').fadeIn();
+
+        //    $('.white-panel').addClass('right-log');
+        //    $('.register-show').addClass('show-log-panel');
+        //    $('.login-show').removeClass('show-log-panel');
+
+        //}
+        //else if ($('#log-reg-show').is(':checked')) {
+        //    $('.register-info-box').fadeIn();
+        //    $('.login-info-box').fadeOut();
+
+        //    $('.white-panel').removeClass('right-log');
+
+        //    $('.login-show').addClass('show-log-panel');
+        //    $('.register-show').removeClass('show-log-panel');
+        //}
     });
 
     function validatePassword() {
